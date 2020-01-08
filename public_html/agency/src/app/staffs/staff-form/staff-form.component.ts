170ae6109e7ffd68c8760be9fe1e480fdb7b990e
ajeesh <ajeeshxp@gmail.com>
2020-01-08 15:14:30 +0530

    add vacancy form
    Signed-off-by: ajeesh <ajeeshxp@gmail.com>

--- public_html/agency/src/app/staffs/staff-form/staff-form.component.ts
+++ public_html/agency/src/app/staffs/staff-form/staff-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-staff-form',
   templateUrl: './staff-form.component.html',
--- public_html/agency/src/app/staffs/staffs.module.ts
+++ public_html/agency/src/app/staffs/staffs.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StaffListComponent } from './staff-list/staff-list.component';
 import { StaffFormComponent } from './staff-form/staff-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [StaffListComponent, StaffFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class StaffsModule { }
