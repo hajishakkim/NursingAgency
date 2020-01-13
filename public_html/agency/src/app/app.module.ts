d16c39baeb8bbda0d9b04b45ec1ad91ea6b9b9d9
hajishakkim369 <hajishakkim47@gmail.com>
2020-01-13 10:55:29 -0800

    form imported
    Signed-off-by: hajishakkim369 <hajishakkim47@gmail.com>

--- public_html/agency/src/app/app.module.ts
+++ public_html/agency/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,7 +45,8 @@ import { ReportsModule } from './reports/reports.module';
     BookingsModule,
     HistoryModule,
     InvoiceModule,
-    ReportsModule
+    ReportsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- public_html/agency/src/app/vacancies/vacancies.module.ts
+++ public_html/agency/src/app/vacancies/vacancies.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VacanciesListComponent } from './vacancies-list/vacancies-list.component';
 import { VacanciesFormComponent } from './vacancies-form/vacancies-form.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [VacanciesListComponent, VacanciesFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+
   ]
 })
 export class VacanciesModule { }
