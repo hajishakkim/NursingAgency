4b2c8d79d63e2e02fc7c014a50848444bad4bf42
hajishakkim369 <hajishakkim47@gmail.com>
2020-02-14 22:28:05 -0800

    candidate form
    Signed-off-by: hajishakkim369 <hajishakkim47@gmail.com>

--- public_html/agency/src/app/candidates/candidates-list/candidates-list.component.ts
+++ public_html/agency/src/app/candidates/candidates-list/candidates-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CandidatesFormComponent } from '../candidates-form/candidates-form.component';
+import { Candidates } from './candidates-list';
+
 declare function setDataTable(options:any,table: string): void;
 @Component({
   selector: 'app-candidates-list',
@@ -6,11 +9,19 @@ declare function setDataTable(options:any,table: string): void;
   styleUrls: ['./candidates-list.component.css']
 })
 export class CandidatesListComponent implements OnInit {
-
+  candidates_data: Candidates[] = [];
   constructor() { }
 
+  @ViewChild('app_candidates_form', {static: false}) app_candidates_form:CandidatesFormComponent;
   ngOnInit() {
     setDataTable(null,'');
+  }
+
+  saveForm(formData: Candidates) {
+    this.candidates_data.push(formData);    
+  }
+  formSubmit(){
+    this.app_candidates_form.saveForm();
   }
 
 }
