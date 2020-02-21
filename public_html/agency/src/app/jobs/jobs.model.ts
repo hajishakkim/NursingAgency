8bb58c486bc5021452653f48cddda4d167180e82
ajeesh <ajeeshxp@gmail.com>
2020-02-21 23:18:43 +0530

    jobs.model +
    Signed-off-by: ajeesh <ajeeshxp@gmail.com>

---
+++ public_html/agency/src/app/jobs/jobs.model.ts
@@ -0,0 +1,8 @@
+export class Jobs {
+  job_id: string = '';
+  job_title: string = '';
+  job_client_id: string = '';
+  job_business_unit_id: string = '';
+  created_date: string = '';
+  job_status: string = '';
+  }
