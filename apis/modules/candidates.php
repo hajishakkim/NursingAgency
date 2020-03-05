824437c7951003272ac556a3cbbb0c9bca2c7ec0
hajishakkim369 <hajishakkim47@gmail.com>
2020-03-05 12:49:20 +0530

    candidate save and validation
    Signed-off-by: hajishakkim369 <hajishakkim47@gmail.com>

--- apis/modules/candidates.php
+++ apis/modules/candidates.php
@@ -6,45 +6,76 @@ if($db){
     $request = json_decode($postdata);
     $data = array();
     try{
-        if($request) $data = (array)$request->data;
+		if($request) $data = (array)$request->data;
+		
     }catch(\Exception $e){}
     if(count($data)>0){
+		//if(!trim($data['candidate_id'])){
+            $sql = "INSERT INTO `candidates` 
+            SET `candidate_fname` 			= ?,
+			`candidate_lname` 			= ?,
+			`candidate_title` 			= ?,
+			`candidate_gender` 			= ?,
+			`candidate_dob` 			= ?,
+			`candidate_address1` 		= ?,
+			`candidate_address2` 		= ?,
+			`candidate_address3` 		= ?,
+			`candidate_country` 		= ?,
+			`candidate_postcode` 		= ?,
+			`candidate_designation` 	= ?,
+			`candidate_dbs_pvg_number` 	= ?,
+			`candidate_dbs_pvg_date` 	= ?,
+			`candidate_staff_id` 		= ?,
+			`candidate_payroll_id` 		= ?,
+			`candidate_email` 			= ?,
+			`candidate_pin_number` 		= ?,
+			`candidate_pin_expiry_date` = ?,
+			`candidate_phone_country_code` 	= ?,
+			`candidate_phone_number` 	= ?,
+			`candidate_mobile_country_code` = ?,
+			`candidate_mobile_phone_number` = ?,
+			`candidate_employment _type` 	= ?,
+			`candidate_status` 			= ?,
+			`candidate_ni_number` 		= ?,
+			`candidate_passport_number` = ?,
+			`candidate_issueing _country` 	= ?,
+			`candidate_visit_type` 		= ?,
+			`created_date` 				= NOW()";
+			
+			$params = array($data['first_name'],
+							$data['middle_name'],
+							$data['sur_name'],
+							$data['gender'],
+							$data['dob'],
+							$data['address1'],
+							$data['address2'],
+							$data['address3'],
+							$data['country'],
+							$data['postcode'],
+							$data['designation'],
+							$data['dbs_pvg_number'],
+							$data['dbs_pvg_issue_date'],
+							$data['staff_id'],
+							$data['payroll_id'],
+							$data['email'],
+							$data['pin'],
+							$data['pin_expire_date'],
+							$data['phone_country_code'],
+							$data['phone'],
+							$data['mobile_country_code'],
+							$data['mobile'],
+							$data['employment_type'],
+							$data['status'],
+							$data['ni_number'],
+							$data['passport_number'],
+							$data['issuing_country'],
+							$data['visa_type']);
+							echo $db->getQuery($sql, $params);
         
-        if(trim($data['vaccancy_id']) == ""){
-            
-            $sql = "INSERT INTO `vaccancy` 
-            (`vaccancy_ref_number`, 
-            `vaccancy_date`, 
-            `vaccancy_client`, 
-            `vaccancy_business_group`, 
-            `vaccancy_shift_type`, 
-            `vaccancy_job`, 
-            `vaccancy_break_time`, 
-            `vaccancy_space`, 
-            `vaccancy_location`, 
-            `vaccancy_details`, 
-            `vaccancy_created_time`, 
-            `vaccancy_updated_time`, 
-            `vaccancy_created_by`, 
-            `vaccancy_updated_by`, 
-            `vaccancy_active`) VALUES 
-            (?,?,?,?,?,?,?,?,?,?,NOW(),NOW(),1,1,1)";
-            //print_r($data);
-            $params = array($data['vaccancy_ref_number'],
-            $data['vaccancy_date'],
-            $data['vaccancy_client'],
-            $data['vaccancy_business_group'],
-            $data['vaccancy_shift_type'],
-            $data['vaccancy_job'],
-            $data['vaccancy_break_time'],
-            $data['vaccancy_space'],
-            $data['vaccancy_location'],
-            $data['vaccancy_details']);
-            
-            $db->add($sql, $params); 
+			$db->add($sql, $params); 
             echo "success";
-        }
-        if(trim($data['vaccancy_id']) != ""){
+        //}
+        if(trim($data['candidate_id']) != ""){
 
             if($data['action'] == "edit"){
             
@@ -56,7 +87,7 @@ if($db){
         }
     }
     else{
-        $sql = "SELECT * FROM `vaccancy`";
+        $sql = "SELECT * FROM `candidates`";
         $result  = $db->select($sql); 
         echo json_encode(array('data'=>$result));
     }
--- public_html/coreui/src/app/candidates/candidates-form/candidates-form.component.ts
+++ public_html/coreui/src/app/candidates/candidates-form/candidates-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import {FormGroup, FormBuilder, Validators,NgForm } from '@angular/forms';
 import * as $ from 'jquery';
 declare function refreshSelectpicker(): void;
 @Component({
@@ -7,8 +7,11 @@ declare function refreshSelectpicker(): void;
   templateUrl: './candidates-form.component.html',
   styleUrls: ['./candidates-form.component.css']
 })
+
+
 export class CandidatesFormComponent implements OnInit {
   form: FormGroup;
+  submitted = false;
   candidate = {
     first_name: '',
     middle_name:'',
@@ -41,45 +44,53 @@ export class CandidatesFormComponent implements OnInit {
 
   };
   @Output() formData = new EventEmitter<Object>();
-  constructor(builder: FormBuilder) {
-    this.form = builder.group({
-      first_name: '',
-      middle_name:'',
-      sur_name:'',
-      gender:'',
-      dob:'',
-      address1:'',
-      address2:'',
-      address3 : '',
-      country:'',
-      postcode:'',
-      designation:'',
-      dbs_pvg_number:'',
-      dbs_pvg_issue_date: '',
-      staff_id:'',
-      payroll_id:'',
-      email:'',
-      pin:'',
-      pin_expire_date:'',
-      phone_country_code:'',
-      phone : '',
-      mobile_country_code:'',
-      mobile:'',
-      employment_type:'',
-      status:'',
-      ni_number:'',
-      passport_number: '',
-      issuing_country:'',
-      visa_type:''
-    })
+
+  constructor(private  builder: FormBuilder) {
+    
   }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      first_name: ['', Validators.required],
+      middle_name:['', Validators.required],
+      sur_name:['', Validators.required],
+      gender:['', Validators.required],
+      dob:['', Validators.required],
+      address1:['', Validators.required],
+      address2:['', Validators.required],
+      address3 : ['', Validators.required],
+      country:['', Validators.required],
+      postcode:['', Validators.required],
+      designation:['', Validators.required],
+      dbs_pvg_number:['', Validators.required],
+      dbs_pvg_issue_date:['', Validators.required],
+      staff_id:['', Validators.required],
+      payroll_id:['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      pin:['', Validators.required],
+      pin_expire_date:['', Validators.required],
+      phone_country_code:['', Validators.required],
+      phone : ['', Validators.required],
+      mobile_country_code:['', Validators.required],
+      mobile:['', Validators.required],
+      employment_type:['', Validators.required],
+      status:['', Validators.required],
+      ni_number:['', Validators.required],
+      passport_number: ['', Validators.required],
+      issuing_country:['', Validators.required],
+      visa_type:['', Validators.required]
+    })
     refreshSelectpicker();
   }
+  
   saveForm(){
-    console.log(this.candidate);
-    //this.formData.emit(this.candidate);
+    refreshSelectpicker();
+    if (this.form.invalid) {
+      this.submitted = true;
+      this.formData.emit(this.candidate);
+    }else{
+      this.formData.emit(this.candidate);
+    }
   }
 
 }
