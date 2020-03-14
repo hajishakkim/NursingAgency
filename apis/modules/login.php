2c96aa4e12e16c5f61755cf28eb88fb5f4b3985c
kaleshkannan <kaleshkannan41@gmail.com>
2020-03-14 21:53:50 +0530

    cnflict changes

--- apis/modules/login.php
+++ apis/modules/login.php
@@ -7,12 +7,8 @@ echo json_encode(
         "email"     =>"kalesh@k.com",
         "userName"  => "kalesh",
         "userId"    => "1",
-<<<<<<< HEAD
         "token"     => "zdasfdsa6r5sad674ads6765",
-        "angency"   => "carebee"
-    ));
-=======
+        "angency"   => "carebee",
         "token"     => "zdasfdsa6r5sad674ads6765"
     )
 );
->>>>>>> 8244d449b6048d344d543cbfff38d06120c16c0e
